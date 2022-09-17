ee3c6c87c24f9e81a086bad3dfc5291540e75aaf
Desha-rgb <mnsar7478@gmail.com>
2022-09-17 05:43:53 +0200

    Practising hooks in this project

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
+import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const cartShownHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const cartHideHandler = () => {
+    setCartIsShown(false);
+  };
+
+  return (
+    <CartProvider>
+      {cartIsShown && <Cart onClose={cartHideHandler} />}
+      <Header onShowCart={cartShownHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
